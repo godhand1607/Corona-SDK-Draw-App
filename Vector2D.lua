061589318362163f0ad5ae6fb3edbe444cd77ca7
godhand1607 <jamcando@gmail.com>
2018-04-19 09:14:25 +0800

    Lint

--- Vector2D.lua
+++ Vector2D.lua
@@ -5,10 +5,11 @@ local mSin = math.sin;
 local mAtan = math.atan;
 local mAtan2 = math.atan2;
 local pi = math.pi;
+
 function Vector2D:new(x, y)  -- The constructor
-  local object = { x = x, y = y }
-  setmetatable(object, { __index = Vector2D })  -- Inheritance
-  return object
+    local object = { x = x, y = y }
+    setmetatable(object, { __index = Vector2D })  -- Inheritance
+    return object
 end
 
 function Vector2D:copy()
@@ -23,13 +24,14 @@ end
 function Vector2D:magnitude()
 	return mSqrt(self.x^2 + self.y^2)
 end
+
 function Vector2D:length()
     return mSqrt(self.x * self.x + self.y *self.y);
 end
+
 function Vector2D:Length(vec)
     return mSqrt(vec.x * vec.x + vec.y *vec.y);
 end
-
 
 function Vector2D:normalize()
 	local temp
@@ -39,14 +41,14 @@ function Vector2D:normalize()
 		self.y = self.y / temp
 	end
 end
+
 function Vector2D:cos_sin()
     self.x = mCos(self.x);
     self.y = mSin(self.y)
-
 end
+
 function Vector2D:dotProduct(vec)
     return self.x * vec.y - self.y * vec.x;
-
 end
 
 function Vector2D:sign(vec)
@@ -57,12 +59,12 @@ function Vector2D:sign(vec)
     else
         return 1;
     end
-
 end
 
 function Vector2D:perpendicular(vec)
     return Vector2D:new(-vec.y , vec.x);
 end
+
 function Vector2D:limit(l)
 	if self.x > l then
 		self.x = l
@@ -72,6 +74,7 @@ function Vector2D:limit(l)
 		self.y = l
 	end
 end
+
 function Vector2D:angle()
 	local ang = mAtan(self.x , self.y);
 	if(self.y< 0 and self.x > 0) then
@@ -84,7 +87,6 @@ function Vector2D:angle()
 
 end
 
-
 function Vector2D:equals(vec)
 	if self.x == vec.x and self.y == vec.y then
 		return true
@@ -92,14 +94,13 @@ function Vector2D:equals(vec)
 		return false
 	end
 end
+
 function Vector2D:truncate(s)
     if #self > s then
         self:normalize();
         self =  self:mult(s);
     end
-
 end
-
 
 function Vector2D:add(vec)
 	self.x = self.x + vec.x
@@ -131,7 +132,7 @@ end
 
 -- Class Methods
 
-function Vector2D:Normalize(vec)	
+function Vector2D:Normalize(vec)
 	local tempVec = Vector2D:new(vec.x,vec.y)
 	local temp
 	temp = tempVec:magnitude()
@@ -139,21 +140,21 @@ function Vector2D:Normalize(vec)
 		tempVec.x = tempVec.x / temp
 		tempVec.y = tempVec.y / temp
 	end
-	
+
 	return tempVec
 end
 
 function Vector2D:Limit(vec,l)
 	local tempVec = Vector2D:new(vec.x,vec.y)
-	
+
 	if tempVec.x > l then
-		tempVec.x = l		
+		tempVec.x = l
 	end
-	
+
 	if tempVec.y > l then
-		tempVec.y = l		
+		tempVec.y = l
 	end
-	
+
 	return tempVec
 end
 
@@ -168,7 +169,7 @@ function Vector2D:Sub(vec1, vec2)
 	local vec = Vector2D:new(0,0)
 	vec.x = vec1.x - vec2.x
 	vec.y = vec1.y - vec2.y
-	
+
 	return vec
 end
 
@@ -176,7 +177,7 @@ function Vector2D:Mult(vec, s)
 	local tempVec = Vector2D:new(0,0)
 	tempVec.x = vec.x * s
 	tempVec.y = vec.y * s
-	
+
 	return tempVec
 end
 
@@ -184,30 +185,28 @@ function Vector2D:Div(vec, s)
 	local tempVec = Vector2D:new(0,0)
 	tempVec.x = vec.x / s
 	tempVec.y = vec.y / s
-	
+
 	return tempVec
 end
+
 function Vector2D:toRad(vec)
     return mAtan2(vec.y,vec.y);
 end
 
-
 function Vector2D:Dist(vec1, vec2)
 	return mSqrt( (vec2.x - vec1.x) + (vec2.y - vec1.y) )
 end
+
 function Vector2D:distance(vec1)
     local dx = vec1.x - self.x;
     local dy = vec1.y - self.y;
     return mSqrt(dx*dx + dy*dy);
-
-
 end
+
 function Vector2D:Distance(vec1, vec2)
     local dx = vec2.x - vec1.x;
     local dy = vec2.y - vec1.y;
     return mSqrt(dx*dx + dy*dy);
-
-
 end
 
 return Vector2D
--- main.lua
+++ main.lua
@@ -1,71 +1,75 @@
 -- MAIN.LUA
 _W = display.contentWidth;
 _H = display.contentHeight;
- local bg = display.newRect(_W,_H,_W,_H)
- bg.x = _W*0.5;
- bg.y  = _H*0.5;
- bg:setFillColor(1,1,1,0)
+
+local bg = display.newRect(_W,_H,_W,_H)
+bg.x = _W*0.5;
+bg.y  = _H*0.5;
+bg:setFillColor(1,1,1,0)
 
 local widget = require "widget"
-local color = { math.random(1,255)/255,math.random(1,255)/255,math.random(1,255)/255,1 } 
+local color = { math.random(1,255)/255,math.random(1,255)/255,math.random(1,255)/255,1 }
 local drawingGroup = require("drawing"):new({
-		Color = color,
-                width = display.contentWidth, -- width of the drawing canvas
-                height = display.contentHeight, -- height of th drawing canvas
-	}); 
-drawingGroup.x = _W*0.5;
-drawingGroup.y = _H*0.5;
+    Color = color,
+    width = display.contentWidth, -- width of the drawing canvas
+    height = display.contentHeight, -- height of th drawing canvas
+});
+drawingGroup.x = _W * 0.5;
+drawingGroup.y = _H * 0.5;
 drawingGroup.width = width;
 drawingGroup.height = height
 -----------------------------------
 -- UNDO & Change Color FUNCTIONS (not required)
 -----------------------------------
 
-local erase = function()
-	drawingGroup.cleanDraw();
+function erase()
+    drawingGroup.cleanDraw();
 end
-local changeColor = function()
-        color = { math.random(1,255)/255,math.random(1,255)/255,math.random(1,255)/255,1 } 
-        drawingGroup.changeColor(color);
-end
-local saveButton = function()
 
-        drawingGroup.saveAsImage("saveBtn.png");
+function changeColor()
+    color = { math.random(1,255)/255,math.random(1,255)/255,math.random(1,255)/255,1 }
+    drawingGroup.changeColor(color);
 end
- 
+
+function save()
+    drawingGroup.saveAsImage("saveBtn.png");
+end
+
 -----------------------------------
 -- UNDO & ERASE BUTTONS (not required)
 -----------------------------------
 
 local eraseButton = widget.newButton{
-        left = display.contentWidth-125,
-        top = display.contentHeight - 50,
-        label = "Erase",
-        width = 100, height = 28,
-        cornerRadius = 8,
-        onRelease = erase
-        }
-local colorButton = widget.newButton{
-        left = display.contentWidth-325,
-        top = display.contentHeight - 50,
-        label = "Change Color",
-        width = 100, height = 28,
-        cornerRadius = 8,
-        onRelease = changeColor
-        }
-local saveButton = widget.newButton{
-        left = display.contentWidth-210,
-        top = display.contentHeight - 50,
-        label = "Save Image",
-        width = 100, height = 28,
-        cornerRadius = 8,
-        onRelease = saveButton
+    left = display.contentWidth-125,
+    top = display.contentHeight - 50,
+    label = "Erase",
+    width = 100, height = 28,
+    cornerRadius = 8,
+    onRelease = erase
 }
- 
- 
-        
+
+local colorButton = widget.newButton{
+    left = display.contentWidth-325,
+    top = display.contentHeight - 50,
+    label = "Change Color",
+    width = 100, height = 28,
+    cornerRadius = 8,
+    onRelease = changeColor
+}
+
+local saveButton = widget.newButton{
+    left = display.contentWidth-210,
+    top = display.contentHeight - 50,
+    label = "Save Image",
+    width = 100, height = 28,
+    cornerRadius = 8,
+    onRelease = save
+}
 
 
 
 
- 
+
+
+
+
